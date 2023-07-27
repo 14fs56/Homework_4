89fab644b805cad473f2c122d723c09733256ab1
Home PC <Home PC@DESKTOP-6JEL7L0>
2023-07-27 21:50:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            sbyte a = -99;
+            ushort b = 55555;
+            uint c = 2394672295;
+            double d = 65.46;
+            char e = 'a';
+            string f = "World";
+        }
+    }
+}
